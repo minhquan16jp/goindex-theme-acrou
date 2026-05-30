c1d92b58e252120f27ffe945f94b8b3a83a09e0c
161718 <99414095+minhquan16jp@users.noreply.github.com>
2026-05-30 11:30:36 +0900

    Update Footer.vue

--- src/views/common/Footer.vue
+++ src/views/common/Footer.vue
@@ -6,8 +6,8 @@
           <div class="tags has-addons">
             <a
               class="tag is-dark"
-              href="https://github.com/Aicirou/goindex-theme-acrou"
-            >goindex-theme-acrou</a>
+              href="https://www.facebook.com/kuan1611/"
+            >Minh Quan</a>
             <span class="tag is-light">
               MIT &nbsp;
               <span class="icon">
